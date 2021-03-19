d01e814e401d77e1bb5a931a998a2ee37a1b65d5
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-03-18 19:38:40 -0700

    Unfortunately, this bit-based cbrt method is not as good.

--- src/main/java/net/adoptopenjdk/bumblebench/examples/OtherCbrtBench.java
+++ src/main/java/net/adoptopenjdk/bumblebench/examples/OtherCbrtBench.java
@@ -42,6 +42,33 @@ public final class OtherCbrtBench extends MicroBench {
 		x  = 0.33333334f*(2f*x + x0/(x*x));
 		return x;
 	}
+
+	/**
+	 * A cube root method by Moroz et al; not recommended.
+	 * I checked this, but it a) needed changes to handle negative numbers without returning
+	 * NaN, and b) has much higher absolute and relative error than the above cbrt().
+	 * Specifically, on one million random floats between -512 (inclusive) and 512 (exclusive),
+	 * the above method gets 0.95888 absolute error and 0.00074 relative error, while this
+	 * Householder method gets 76.51524 absolute error and 0.01686 relative error.
+	 * From https://res.mdpi.com/d_attachment/energies/energies-14-01058/article_deploy/energies-14-01058-v2.pdf
+	 * @param x a float to get the cube root for
+	 * @return the cube root, approximately
+	 */
+	public static float cbrtHouseholder(float x) {
+		final float k1 = 1.752319676f;
+		final float k2 = 1.2509524245f;
+		final float k3 = 0.5093818292f;
+		int ix = Float.floatToRawIntBits(x);
+		final int sign = ix&0x80000000;
+		ix &= 0x7FFFFFFF;
+		// this next line is different from what the paper used, but
+		// "i * 0xAAAAAAABL >>> 33" is identical to "i / 3" for positive i.
+		float y = Float.intBitsToFloat(0x548c2b4b-(int)(ix * 0xAAAAAAABL >>> 33)^sign);
+		final float c = x*y*y*y;
+		y*=(k1-c*(k2-k3*c));
+		final float d = x*y*y;
+		return d*(1.0f+0.333333333333f*(1.0f-d*y));
+	}
 	protected long doBatch (long numIterations) throws InterruptedException {
 		float sum = 0.1f;
 		final float shrink = 1.6180339887498949f / numIterations;
@@ -52,15 +79,22 @@ public final class OtherCbrtBench extends MicroBench {
 
 	public static void main(String[] args) {
 		LaserRandom random = new LaserRandom(12345, 6789);
-		double sumError = 0.0, relativeError = 0.0;
+		double sumError = 0.0, relativeError = 0.0, sumErrorH = 0.0, relativeErrorH = 0.0;
 		for (int i = 0; i < 1000000; i++) {
 			float r = (random.nextFloat() - 0.5f) * random.next(10);
 			float accurate = (float) Math.cbrt(r);
 			float approx = cbrt(r);
+			float approxH = cbrtHouseholder(r);
 			float error = accurate - approx;
 			relativeError += error;
 			sumError += Math.abs(error);
+			float errorH = accurate - approxH;
+			relativeErrorH += errorH;
+			sumErrorH += Math.abs(errorH);
 		}
+		System.out.println("Newton-Raphson:");
 		System.out.printf("Sum Error: %5.5f (averaged, %5.5f), Rel Error: %5.5f (averaged, %5.5f)", sumError, sumError / 0x100000, relativeError, relativeError / 0x100000);
+		System.out.println("\nHouseholder:");
+		System.out.printf("Sum Error: %5.5f (averaged, %5.5f), Rel Error: %5.5f (averaged, %5.5f)", sumErrorH, sumErrorH / 0x100000, relativeErrorH, relativeErrorH / 0x100000);
 	}
 }
