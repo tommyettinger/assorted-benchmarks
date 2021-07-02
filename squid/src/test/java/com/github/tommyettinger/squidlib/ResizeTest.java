b76bd6133dcb6463eb4a1b58caef667ddcc732c8
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-07-01 20:47:54 -0700

    Test Kryo's CuckooObjectMap.
    It does fine with dictionary Strings. This also updates SquidSquad and SquidLib, at least in the squid module.

--- squid/src/test/java/com/github/tommyettinger/squidlib/ResizeTest.java
+++ squid/src/test/java/com/github/tommyettinger/squidlib/ResizeTest.java
@@ -54,6 +54,17 @@ public class ResizeTest {
                     }
                 }
             }
+            {
+                CuckooObjectMap<String, Object> map = new CuckooObjectMap<>(16, 0.8f);
+                int cap = map.capacity;
+                for (int i = 0; i < size; i++) {
+                    map.put(arr[i], null);
+                    if(cap != map.capacity){
+                        cap = map.capacity;
+                        System.out.println("Resized on " + i + ": " + arr[i]);
+                    }
+                }
+            }
         }
     }
 }
