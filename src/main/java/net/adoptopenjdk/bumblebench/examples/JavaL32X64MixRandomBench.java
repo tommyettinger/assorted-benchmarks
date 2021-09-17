3d23780372f0446deda73fbfd11c75130edaec0f
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-09-16 18:35:15 -0700

    Add benchmarks for almost all of the new Java 17 RNGs.
    Speed results are incoming. This moves the project up to Java 17; I'm using SAP Machine 17 on my machine.

---
+++ src/main/java/net/adoptopenjdk/bumblebench/examples/JavaL32X64MixRandomBench.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+package net.adoptopenjdk.bumblebench.examples;
+
+import net.adoptopenjdk.bumblebench.core.MicroBench;
+
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
+
+/**
+ * Windows 10, 10th gen i7 mobile hexacore at 2.6 GHz:
+ * <br>
+ * Java 8, HotSpot:
+ * <br>
+ *
+ * <br>
+ * With Java 15, OpenJ9, same hardware:
+ * <br>
+ *
+ * <br>
+ * HotSpot Java 16:
+ * <br>
+ *
+ */
+/*
+L128X1024MixRandom
+L128X128MixRandom
+L128X256MixRandom
+L32X64MixRandom
+L64X1024MixRandom
+L64X128MixRandom
+L64X128StarStarRandom
+L64X256MixRandom
+*/
+public final class JavaL32X64MixRandomBench extends MicroBench {
+
+	protected long doBatch(long numIterations) throws InterruptedException {
+		RandomGeneratorFactory<RandomGenerator> factory = RandomGeneratorFactory.of("L32X64MixRandom");
+		RandomGenerator rng = factory.create(0x12345678);
+		long sum = 0L;
+		for (long i = 0; i < numIterations; i++)
+			sum += rng.nextLong();
+		return numIterations;
+	}
+}
