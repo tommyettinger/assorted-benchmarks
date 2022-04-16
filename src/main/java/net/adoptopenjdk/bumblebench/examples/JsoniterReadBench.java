6df92a3e26c3b360f7d59ff211c85fafcfc262f1
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-04-16 03:16:55 -0700

    Try out Jsoniter; can't get it to read.
    BumbleBench just hangs and uses a lot of RAM when I try to run JsoniterReadBenchmark (after generating jsoniter.json with JsoniterWriteBench).

---
+++ src/main/java/net/adoptopenjdk/bumblebench/examples/JsoniterReadBench.java
@@ -0,0 +1,74 @@
+/*******************************************************************************
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+package net.adoptopenjdk.bumblebench.examples;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Files;
+import com.badlogic.gdx.math.Vector2;
+import com.jsoniter.JsonIterator;
+import com.jsoniter.spi.Config;
+import com.jsoniter.spi.DecodingMode;
+import com.jsoniter.spi.TypeLiteral;
+import net.adoptopenjdk.bumblebench.core.MiniBench;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Java 17:
+ * <br>
+ *
+ */
+public final class JsoniterReadBench extends MiniBench {
+	@Override
+	protected int maxIterationsPerLoop() {
+		return 1007;
+	}
+
+	@Override
+	protected long doBatch(long numLoops, int numIterationsPerLoop) throws InterruptedException {
+		String data = new Lwjgl3Files().local("jsoniter.json").readString();
+		HashMap<String, ArrayList<Vector2>> big;
+		Config cfg = new Config.Builder()
+				.omitDefaultValue(true)
+				.build();
+		JsonIterator.setMode(DecodingMode.DYNAMIC_MODE_AND_MATCH_FIELD_WITH_HASH);
+		TypeLiteral<HashMap<String, ArrayList<Vector2>>> tl = new TypeLiteral<HashMap<String, ArrayList<Vector2>>>(){};
+		int counter = 0;
+		for (long i = 0; i < numLoops; i++) {
+			for (int j = 0; j < numIterationsPerLoop; j++) {
+				startTimer();
+				big = JsonIterator.deserialize(cfg, data, tl);
+				counter += big.size();
+				pauseTimer();
+			}
+		}
+		return numLoops * numIterationsPerLoop;
+	}
+	public static void main(String[] args) {
+		byte[] data = new Lwjgl3Files().local("jsoniter.json").readBytes();
+		Config cfg = new Config.Builder()
+				.omitDefaultValue(true)
+				.build();
+		JsonIterator.setMode(DecodingMode.DYNAMIC_MODE_AND_MATCH_FIELD_WITH_HASH);
+
+		HashMap<String, ArrayList<Vector2>> big = JsonIterator.deserialize(cfg, data,
+				new TypeLiteral<HashMap<String, ArrayList<Vector2>>>(){});
+		System.out.println(big.get("whoremongers")); // big pimpin' King James
+	}
+}
+
