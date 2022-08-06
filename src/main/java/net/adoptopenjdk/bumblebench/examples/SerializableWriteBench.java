02cd5eeb4e9bf1d3c5cf567d0f41cf8a428a7be4
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-08-05 18:15:03 -0700

    Using Serializable is much slower than Kryo.
    By quite a lot. I could be doing something wrong, but this looks pretty dire for java.io.Serializable .
    This also updates Gradle to 7.5.1 .

---
+++ src/main/java/net/adoptopenjdk/bumblebench/examples/SerializableWriteBench.java
@@ -0,0 +1,114 @@
+/*******************************************************************************
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+package net.adoptopenjdk.bumblebench.examples;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.ObjectSet;
+import com.github.tommyettinger.random.FourWheelRandom;
+import net.adoptopenjdk.bumblebench.core.MiniBench;
+import squidpony.StringKit;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Java 17:
+ * <br>
+ * SerializableWriteBench score: 76.327919 (76.33 433.5%)
+ *                    uncertainty:   0.7%
+ */
+public final class SerializableWriteBench extends MiniBench {
+	@Override
+	protected int maxIterationsPerLoop() {
+		return 1000007;
+	}
+
+	@Override
+	protected long doBatch(long numLoops, int numIterationsPerLoop) throws InterruptedException {
+		String book = "";
+		try {
+			book = new String(Files.readAllBytes(Paths.get("res/bible_only_words.txt")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		final String[] words = StringKit.split(book, " ");
+		ObjectSet<String> unique = ObjectSet.with(words);
+		HashMap<String, ArrayList<Vector2>> big = new HashMap<>(unique.size);
+		FourWheelRandom random = new FourWheelRandom(12345);
+		for (String u : unique) {
+			big.put(u, new ArrayList<>(Arrays.asList(
+					new Vector2(random.nextExclusiveFloat() - 0.5f, random.nextExclusiveFloat() - 0.5f),
+					new Vector2(random.nextExclusiveFloat() - 0.5f, random.nextExclusiveFloat() - 0.5f),
+					new Vector2(random.nextExclusiveFloat() - 0.5f, random.nextExclusiveFloat() - 0.5f)
+			)));
+		}
+
+		int counter = 0;
+		try {
+			for (long i = 0; i < numLoops; i++) {
+				for (int j = 0; j < numIterationsPerLoop; j++) {
+					ByteArrayOutputStream baos = new ByteArrayOutputStream();
+					ObjectOutputStream output = null;
+					startTimer();
+					output = new ObjectOutputStream(baos);
+					output.writeObject(big);
+					output.flush();
+					counter += baos.size();
+					pauseTimer();
+				}
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return numLoops * numIterationsPerLoop;
+	}
+
+	public static void main(String[] args) {
+		String book = "";
+		try {
+			book = new String(Files.readAllBytes(Paths.get("res/bible_only_words.txt")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		final String[] words = StringKit.split(book, " ");
+		ObjectSet<String> unique = ObjectSet.with(words);
+		HashMap<String, ArrayList<Vector2>> big = new HashMap<>(unique.size);
+		FourWheelRandom random = new FourWheelRandom(12345);
+		for(String u : unique){
+			big.put(u, new ArrayList<>(Arrays.asList(
+					new Vector2(random.nextExclusiveFloat() - 0.5f, random.nextExclusiveFloat() - 0.5f),
+					new Vector2(random.nextExclusiveFloat() - 0.5f, random.nextExclusiveFloat() - 0.5f),
+					new Vector2(random.nextExclusiveFloat() - 0.5f, random.nextExclusiveFloat() - 0.5f)
+			)));
+		}
+
+		System.out.println("There are " + big.size() + " keys in the Map.");
+
+		try {
+			ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("ser.dat"));
+			output.writeObject(big);
+			output.flush();
+			output.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
+
