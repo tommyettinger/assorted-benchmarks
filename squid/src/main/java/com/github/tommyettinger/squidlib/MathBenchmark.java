711f6b4eda02d17099ec3059c3c93888c6ef114c
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-09-05 20:36:02 -0700

    Benchmark Java 8; Soonts' way isn't as good here.

--- squid/src/main/java/com/github/tommyettinger/squidlib/MathBenchmark.java
+++ squid/src/main/java/com/github/tommyettinger/squidlib/MathBenchmark.java
@@ -176,6 +176,14 @@ import java.util.concurrent.TimeUnit;
  * MathBenchmark.measureMathTan    avgt    5  17.659 ± 1.176  ns/op
  * MathBenchmark.measureSoontsTan  avgt    5   7.824 ± 0.646  ns/op
  * </pre>
+ * <br>
+ * And on Java 8:
+ * <pre>
+ * Benchmark                       Mode  Cnt   Score   Error  Units
+ * MathBenchmark.measureDivideTan  avgt    5   4.605 ± 0.219  ns/op
+ * MathBenchmark.measureMathTan    avgt    5  51.494 ± 2.440  ns/op
+ * MathBenchmark.measureSoontsTan  avgt    5  13.219 ± 0.858  ns/op
+ * </pre>
  */
 
 @State(Scope.Thread)
