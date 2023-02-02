881a9a70a89f27f1afca2e56aeea90c028ef9625
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-02-01 20:32:37 -0800

    Surprise! Smoother is faster than Smooth.
    This of course assumes the sine table has been used a lot, and is in cache -- though if it isn't, sin() probably isn't called enough to be performance-critical.

---
+++ src/main/java/net/adoptopenjdk/bumblebench/examples/DigitalSinSmootherBench.java
@@ -0,0 +1,100 @@
+/*******************************************************************************
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+package net.adoptopenjdk.bumblebench.examples;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.github.tommyettinger.digital.TrigTools;
+import net.adoptopenjdk.bumblebench.core.MicroBench;
+
+/**
+ * Windows 10, 10th gen i7 mobile hexacore at 2.6 GHz:
+ * <br>
+ * HotSpot Java 8:
+ * <br>
+ * DigitalSinSmootherBench score: 84239928.000000 (84.24M 1824.9%)
+ *                     uncertainty:   0.2%
+ * <br>
+ * OpenJ9 Java 15:
+ * <br>
+ * DigitalSinSmootherBench score: 84092720.000000 (84.09M 1824.7%)
+ *                     uncertainty:   1.5%
+ * <br>
+ * HotSpot Java 16 (AdoptOpenJDK):
+ * <br>
+ * DigitalSinSmootherBench score: 84961760.000000 (84.96M 1825.8%)
+ *                     uncertainty:   0.5%
+ * <br>
+ * HotSpot Java 17 (Adoptium):
+ * <br>
+ * DigitalSinSmootherBench score: 84208928.000000 (84.21M 1824.9%)
+ *                     uncertainty:   0.7%
+ * <br>
+ * GraalVM Java 17:
+ * <br>
+ * DigitalSinSmootherBench score: 84736304.000000 (84.74M 1825.5%)
+ *                     uncertainty:   0.2%
+ * <br>
+ * HotSpot Java 18 (Adoptium):
+ * <br>
+ * DigitalSinSmootherBench score: 84507312.000000 (84.51M 1825.2%)
+ *                     uncertainty:   0.8%
+ * <br>
+ * HotSpot Java 19 (BellSoft):
+ * <br>
+ * DigitalSinSmootherBench score: 84595008.000000 (84.60M 1825.3%)
+ *                     uncertainty:   0.1%
+ */
+public final class DigitalSinSmootherBench extends MicroBench {
+
+	protected long doBatch(long numIterations) throws InterruptedException {
+		float sum = 0.1f;
+		final float shrink = MathUtils.PI * 8f / numIterations;
+		for (long i = 0; i < numIterations; i++)
+			sum -= TrigTools.sinSmoother((sum + i) * shrink);
+		return numIterations;
+	}
+}
+
+/*
+ * Windows 10, 10th gen i7 mobile hexacore at 2.6 GHz:
+ * <br>
+ * HotSpot Java 8:
+ * <br>
+ *
+ * <br>
+ * OpenJ9 Java 15:
+ * <br>
+ *
+ * <br>
+ * HotSpot Java 16 (AdoptOpenJDK):
+ * <br>
+ *
+ * <br>
+ * HotSpot Java 17 (Adoptium):
+ * <br>
+ *
+ * <br>
+ * GraalVM Java 17:
+ * <br>
+ *
+ * <br>
+ * HotSpot Java 18 (Adoptium):
+ * <br>
+ *
+ * <br>
+ * HotSpot Java 19 (BellSoft):
+ * <br>
+ *
+ */
