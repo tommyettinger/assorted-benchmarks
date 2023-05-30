ab6943a6a5e82dd229f59e267c559f1150251c8d
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-05-29 21:45:14 -0700

    StaunchRandom is better, still... not great.

--- src/main/java/net/adoptopenjdk/bumblebench/examples/StaunchRandomBench.java
+++ src/main/java/net/adoptopenjdk/bumblebench/examples/StaunchRandomBench.java
@@ -22,40 +22,48 @@ import net.adoptopenjdk.bumblebench.core.MicroBench;
  * <br>
  * HotSpot Java 8:
  * <br>
- *
+ * StaunchRandomBench score: 210687760.000000 (210.7M 1916.6%)
+ *                uncertainty:   0.7%
  * <br>
  * OpenJ9 Java 15:
  * <br>
- *
+ * StaunchRandomBench score: 1807417600.000000 (1.807G 2131.5%)
+ *               uncertainty:   3.6%
  * <br>
  * HotSpot Java 16 (AdoptOpenJDK):
  * <br>
- *
+ *  StaunchRandomBench score: 243313952.000000 (243.3M 1931.0%)
+ *                uncertainty:   1.0%
  * <br>
  * HotSpot Java 17 (Adoptium):
  * <br>
- *
+ * StaunchRandomBench score: 248051392.000000 (248.1M 1932.9%)
+ *                uncertainty:   1.5%
  * <br>
  * GraalVM Java 17:
  * <br>
- *
+ * StaunchRandomBench score: 610656384.000000 (610.7M 2023.0%)
+ *                uncertainty:   2.1%
  * <br>
  * OpenJ9 Java 17 (Semeru):
  * <br>
- *
+ * StaunchRandomBench score: 831231296.000000 (831.2M 2053.8%)
+ *               uncertainty:   1.0%
  * <br>
  * HotSpot Java 18 (Adoptium):
  * <br>
- *
+ * StaunchRandomBench score: 247760144.000000 (247.8M 1932.8%)
+ *                uncertainty:   0.2%
  * <br>
  * HotSpot Java 19 (BellSoft):
  * <br>
- *
+ * StaunchRandomBench score: 243001280.000000 (243.0M 1930.9%)
+ *                uncertainty:   0.8%
  */
-public final class WrenRandomBench extends MicroBench {
+public final class StaunchRandomBench extends MicroBench {
 
 	protected long doBatch(long numIterations) throws InterruptedException {
-		WrenRandom rng = new WrenRandom(0x12345678);
+		StaunchRandom rng = new StaunchRandom(0x12345678);
 		long sum = 0L;
 		for (long i = 0; i < numIterations; i++)
 			sum += rng.nextLong();
