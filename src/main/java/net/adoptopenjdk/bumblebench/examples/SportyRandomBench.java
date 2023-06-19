563078e27ea5e0677633faa5dd699cb570f4cbb5
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-06-18 21:41:28 -0700

    Update SportyRandom and its benchmarks.

--- src/main/java/net/adoptopenjdk/bumblebench/examples/SportyRandomBench.java
+++ src/main/java/net/adoptopenjdk/bumblebench/examples/SportyRandomBench.java
@@ -22,6 +22,61 @@ import net.adoptopenjdk.bumblebench.core.MicroBench;
  * <br>
  * HotSpot Java 8:
  * <br>
+ * SportyRandomBench score: 601385984.000000 (601.4M 2021.5%)
+ *               uncertainty:   1.4%
+ * <br>
+ * OpenJ9 Java 15:
+ * <br>
+ * SportyRandomBench score: 1528944768.000000 (1.529G 2114.8%)
+ *               uncertainty:   0.6%
+ * <br>
+ * HotSpot Java 16 (AdoptOpenJDK):
+ * <br>
+ * SportyRandomBench score: 493411776.000000 (493.4M 2001.7%)
+ *               uncertainty:   4.9%
+ * <br>
+ * HotSpot Java 17 (Adoptium):
+ * <br>
+ * SportyRandomBench score: 559604928.000000 (559.6M 2014.3%)
+ *               uncertainty:   0.4%
+ * <br>
+ * GraalVM Java 17:
+ * <br>
+ * SportyRandomBench score: 673767040.000000 (673.8M 2032.8%)
+ *               uncertainty:   1.0%
+ * <br>
+ * OpenJ9 Java 17 (Semeru):
+ * <br>
+ * SportyRandomBench score: 778007488.000000 (778.0M 2047.2%)
+ *               uncertainty:   0.6%
+ * <br>
+ * HotSpot Java 18 (Adoptium):
+ * <br>
+ * SportyRandomBench score: 559621952.000000 (559.6M 2014.3%)
+ *               uncertainty:   0.2%
+ * <br>
+ * HotSpot Java 19 (BellSoft):
+ * <br>
+ * SportyRandomBench score: 562094656.000000 (562.1M 2014.7%)
+ *               uncertainty:   0.9%
+ */
+public final class SportyRandomBench extends MicroBench {
+
+	protected long doBatch(long numIterations) throws InterruptedException {
+		SportyRandom rng = new SportyRandom(0x12345678);
+		long sum = 0L;
+		for (long i = 0; i < numIterations; i++)
+			sum += rng.nextLong();
+		return numIterations;
+	}
+}
+// EARLIER RESULTS
+// These used 64-bit states B/C/D instead of always-odd states for those, and forced them to odd during generation.
+/*
+ * Windows 10, 10th gen i7 mobile hexacore at 2.6 GHz:
+ * <br>
+ * HotSpot Java 8:
+ * <br>
  * SportyRandomBench score: 465400832.000000 (465.4M 1995.8%)
  *               uncertainty:   0.5%
  * <br>
@@ -60,16 +115,8 @@ import net.adoptopenjdk.bumblebench.core.MicroBench;
  * SportyRandomBench score: 507827200.000000 (507.8M 2004.6%)
  *               uncertainty:   2.6%
  */
-public final class SportyRandomBench extends MicroBench {
 
-	protected long doBatch(long numIterations) throws InterruptedException {
-		SportyRandom rng = new SportyRandom(0x12345678);
-		long sum = 0L;
-		for (long i = 0; i < numIterations; i++)
-			sum += rng.nextLong();
-		return numIterations;
-	}
-}
+
 // TEMPLATE
 /*
  * Windows 10, 10th gen i7 mobile hexacore at 2.6 GHz:
