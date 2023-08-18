01f3f0618455aade9dfa4e6604d784bcd83c1278
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-08-17 21:37:05 -0700

    Interesting results from Graal re: Random !
    This was a bolt from the blue; Random.nextLong() is about 6x faster on Graal 17 than it is on any HotSpot version. It's still slower than Xoshiro256MX3 by a fair margin, and only generates at about 2/9 the speed of AceRandom on the same Graal version.

--- src/main/java/net/adoptopenjdk/bumblebench/examples/JavaRandomLongBench.java
+++ src/main/java/net/adoptopenjdk/bumblebench/examples/JavaRandomLongBench.java
@@ -23,6 +23,63 @@ import java.util.Random;
  * <br>
  * Windows 10, 10th gen i7 mobile hexacore at 2.6 GHz:
  * <br>
+ * HotSpot Java 8:
+ * <br>
+ * JavaRandomLongBench score: 61803784.000000 (61.80M 1793.9%)
+ *                 uncertainty:   1.3%
+ * <br>
+ * OpenJ9 Java 15:
+ * <br>
+ * JavaRandomLongBench score: 61998444.000000 (62.00M 1794.3%)
+ *                 uncertainty:   0.6%
+ * <br>
+ * HotSpot Java 16 (AdoptOpenJDK):
+ * <br>
+ * JavaRandomLongBench score: 62048896.000000 (62.05M 1794.3%)
+ *                 uncertainty:   1.9%
+ * <br>
+ * HotSpot Java 17 (Adoptium):
+ * <br>
+ * JavaRandomLongBench score: 63836576.000000 (63.84M 1797.2%)
+ *                 uncertainty:   0.2%
+ * <br>
+ * GraalVM Java 17:
+ * <br>
+ * JavaRandomLongBench score: 403292800.000000 (403.3M 1981.5%)
+ *                 uncertainty:   0.7%
+ * <br>
+ * OpenJ9 Java 17 (Semeru):
+ * <br>
+ * JavaRandomLongBench score: 61527740.000000 (61.53M 1793.5%)
+ *                 uncertainty:   0.5%
+ * <br>
+ * HotSpot Java 18 (Adoptium):
+ * <br>
+ * JavaRandomLongBench score: 63727604.000000 (63.73M 1797.0%)
+ *                 uncertainty:   0.6%
+ * <br>
+ * HotSpot Java 19 (BellSoft):
+ * <br>
+ * JavaRandomLongBench score: 63558280.000000 (63.56M 1796.7%)
+ *                 uncertainty:   0.2%
+ */
+public final class JavaRandomLongBench extends MicroBench {
+
+	protected long doBatch(long numIterations) throws InterruptedException {
+		Random rng = new Random(0x12345678);
+		long sum = 0L;
+		for (long i = 0; i < numIterations; i++)
+			sum += rng.nextLong();
+		return numIterations;
+	}
+}
+
+// Old results
+/*
+ * This tests long generation even though Random is not very fast at it.
+ * <br>
+ * Windows 10, 10th gen i7 mobile hexacore at 2.6 GHz:
+ * <br>
  * Java 8, HotSpot:
  * <br>
  * JavaRandomLongBench score: 58726960.000000 (58.73M 1788.8%)
@@ -38,13 +95,3 @@ import java.util.Random;
  * JavaRandomLongBench score: 58880216.000000 (58.88M 1789.1%)
  *                 uncertainty:   0.3%
  */
-public final class JavaRandomLongBench extends MicroBench {
-
-	protected long doBatch(long numIterations) throws InterruptedException {
-		Random rng = new Random(0x12345678);
-		long sum = 0L;
-		for (long i = 0; i < numIterations; i++)
-			sum += rng.nextLong();
-		return numIterations;
-	}
-}
