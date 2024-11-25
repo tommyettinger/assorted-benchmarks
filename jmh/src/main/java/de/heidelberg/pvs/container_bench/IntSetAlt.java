c29276fd409840288962506e59dbdf988ff752bc
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-11-24 17:26:13 -0800

    About as simple as it gets, but faster.

--- jmh/src/main/java/de/heidelberg/pvs/container_bench/IntSetAlt.java
+++ jmh/src/main/java/de/heidelberg/pvs/container_bench/IntSetAlt.java
@@ -185,7 +185,7 @@ public class IntSetAlt implements PrimitiveSet.SetOfInt {
 	 * @return an index between 0 and {@link #mask} (both inclusive)
 	 */
 	protected int place (int item) {
-		return BitConversion.imul(item, hashMultiplier) >>> shift;
+		return (item ^ item >>> 16) & mask;
 	}
 
 	/**
