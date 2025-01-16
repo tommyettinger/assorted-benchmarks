e05846c9ec6457b8f6cb0b47e805bd308c268460
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-01-15 20:20:16 -0800

    For uniformly distributed keys, just masking is...
    ... a tiny bit faster for long keys. For pathologically bad distributions, it will be astoundingly slow.

--- jmh/src/main/java/de/heidelberg/pvs/container_bench/factories/JDKGDXDSMapLong2ObjFact.java
+++ jmh/src/main/java/de/heidelberg/pvs/container_bench/factories/JDKGDXDSMapLong2ObjFact.java
@@ -6,8 +6,14 @@ import com.github.tommyettinger.ds.LongObjectOrderedMap;
 import java.util.function.Supplier;
 
 public enum JDKGDXDSMapLong2ObjFact {
-	JDKGDXDS_L2O_PRIMITIVE_HASH(() -> new LongObjectMap<>(8, LoadFactor.LOAD_FACTOR)), //
-	JDKGDXDS_L2O_PRIMITIVE_ORDERED(() -> new LongObjectOrderedMap<>(8, LoadFactor.LOAD_FACTOR)), //
+	JDKGDXDS_L2O_PRIMITIVE_HASH(() -> new LongObjectMap<>(8, LoadFactor.LOAD_FACTOR)),
+	JDKGDXDS_L2O_PRIMITIVE_ORDERED(() -> new LongObjectOrderedMap<>(8, LoadFactor.LOAD_FACTOR)),
+	JDKGDXDS_L2O_PRIMITIVE_UNMIXED(() -> new LongObjectMap<Object>(8, LoadFactor.LOAD_FACTOR) {
+		@Override
+		protected int place(long item) {
+			return (int)(item) & mask;
+		}
+	}),
 	;
 
 	public final Supplier<LongObjectMap<Object>> maker;
