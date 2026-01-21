2bc534be0871ca95f6392cb0f41c07bca27ab70a
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-01-21 14:42:21 -0800

    OK... interesting... Is it the constant, or final?

---
+++ jmh/src/main/java/com/github/tommyettinger/ds/IntIntMapUnchanging.java
@@ -0,0 +1,34 @@
+package com.github.tommyettinger.ds;
+
+import com.github.tommyettinger.digital.BitConversion;
+
+public class IntIntMapUnchanging extends IntIntMap{
+    public IntIntMapUnchanging() {
+        super();
+    }
+
+    public IntIntMapUnchanging(int initialCapacity) {
+        super(initialCapacity);
+    }
+
+    public IntIntMapUnchanging(int initialCapacity, float loadFactor) {
+        super(initialCapacity, loadFactor);
+    }
+
+    public IntIntMapUnchanging(IntIntMap map) {
+        super(map);
+    }
+
+    public IntIntMapUnchanging(int[] keys, int[] values) {
+        super(keys, values);
+    }
+
+    public IntIntMapUnchanging(PrimitiveCollection.OfInt keys, PrimitiveCollection.OfInt values) {
+        super(keys, values);
+    }
+
+    @Override
+    protected final int place(int item) {
+        return BitConversion.imul(item, 0x9E3779B1) >>> shift;
+    }
+}
--- jmh/src/main/java/de/heidelberg/pvs/container_bench/factories/JDKGDXDSMapInt2IntFact.java
+++ jmh/src/main/java/de/heidelberg/pvs/container_bench/factories/JDKGDXDSMapInt2IntFact.java
@@ -1,11 +1,15 @@
 package de.heidelberg.pvs.container_bench.factories;
 
 import com.github.tommyettinger.ds.IntIntMap;
+import com.github.tommyettinger.ds.IntIntMapUnchanging;
+import com.github.tommyettinger.ds.IntIntOrderedMap;
 
 import java.util.function.Supplier;
 
 public enum JDKGDXDSMapInt2IntFact {
 	JDKGDXDS_I2I_HASH(() -> new IntIntMap(16, LoadFactor.LOAD_FACTOR)), //
+	JDKGDXDS_I2I_UNCHANGING(() -> new IntIntMapUnchanging(16, LoadFactor.LOAD_FACTOR)), //
+	JDKGDXDS_I2I_ORDERED(() -> new IntIntOrderedMap(16, LoadFactor.LOAD_FACTOR)), //
 	;
 
 	public final Supplier<IntIntMap> maker;
